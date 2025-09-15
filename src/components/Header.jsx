96c738d25ed10972a65f32b72505ed4564d662fb
RKPatel-1996 <rrohit3rock@gmail.com>
2025-09-15 10:49:56 +0530

    feat: build functional header and applied some global settings

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,34 @@
+// src/components/Header.jsx
+import { NavLink } from "react-router-dom";
+import "./Header.css"; // Import the CSS file
+
 function Header() {
   return (
-    <header style={{ padding: "20px", backgroundColor: "#f3f3f3" }}>
-      Header
+    <header className="app-header">
+      <NavLink to="/" className="site-title">
+        BioMicro.Interact
+      </NavLink>
+      <nav className="main-nav">
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/blogs">Blogs</NavLink>
+          </li>
+          <li>
+            <NavLink to="/video">Video</NavLink>
+          </li>
+          <li>
+            <NavLink to="/support">Support</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About Me</NavLink>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
+
 export default Header;
